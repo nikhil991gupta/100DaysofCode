4237bf89863716daef06ab3b7dce1b5439f08c5e
nikhil991gupta <nikhil991gupta@gmail.com>
2022-10-03 01:46:42 +0530

    DAY 5

---
+++ Dsa_Bubble_Sort.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Dsa_Bubble_Sort {
+
+    int[] BubbleSort(int arr[],int n){
+        for(int i =1 ; i <= n-1 ; i++){
+            for(int j = n ; j >= i+1 ; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    void PrintArray(int arr[]){
+        for(int i = 1; i<= arr.length-1 ; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+class Dsa_BubbleSort_Exe{
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter how many values you want to enter : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n+1];
+
+        for(int i = 1 ; i <= n; i++){
+            System.out.print("No. "+ (i) +" ");
+            arr[i]=sc.nextInt();
+        }
+
+        Dsa_Bubble_Sort DB = new Dsa_Bubble_Sort();
+        DB.PrintArray(DB.BubbleSort(arr, n));
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+        System.out.println("\n");
+        System.out.println("Time for execution"+elapsedTime+"millis");
+    }
+}
