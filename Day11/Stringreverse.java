f2c4277c589fde67de1475e17c5826e8e175da7d
nikhil991gupta <nikhil991gupta@gmail.com>
2022-10-09 22:38:41 +0530

    DAY11 reverse string

---
+++ Day11/Stringreverse.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Stringreverse {
+
+    public static void main(String[] args) {
+        //char ch;
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+       
+        
+       StringBuilder sb=new StringBuilder(A);  
+    sb.reverse();  
+    String rev=sb.toString();  
+    if(A.equals(rev)){  
+        System.out.println("Yes");
+    }
+    else
+        System.out.println("No");
+    }
+       
+        
+
+    {
+        
+    }
+           // System.out.print(try1[i]);
+    }
+   
+  
+        
+    
+
+
+
