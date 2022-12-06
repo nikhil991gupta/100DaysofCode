f685e257e99b233addd84ca8ccbaa902bcd7d4bf
nikhil991gupta <nikhil991gupta@gmail.com>
2022-12-06 13:11:04 +0530

    Java Generics

---
+++ DAY57/generics.java
@@ -0,0 +1,37 @@
+package DAY57;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+class Printer
+{
+   //Write your code here
+ public static < E > void printArray( E[] inputArray )
+   {
+      // Display array elements              
+         for ( E element : inputArray ){        
+            System.out.println(element);
+         }
+    }
+}
+
+public class generics {
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
