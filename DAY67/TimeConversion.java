0fa44a2c2f2d6dbc298e05746a04a0cc922a7f5c
nikhil991gupta <nikhil991gupta@gmail.com>
2022-12-16 23:23:28 +0530

    Time Conversion

---
+++ DAY67/TimeConversion.java
@@ -0,0 +1,47 @@
+package DAY67;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+    int hour = Integer.parseInt(s.substring(0, 2)) % 12;
+    if (s.endsWith("PM"))
+        hour += 12;
+    return String.format("%02d", hour) + s.substring(2, 8);
+
+    }
+
+}
+
+public class TimeConversion {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
