3ae4c8785083ca83302d0e21b5edca891d879810
nikhil991gupta <nikhil991gupta@gmail.com>
2022-12-22 22:25:11 +0530

    Divisible Sum Pairs

---
+++ DAY73/DivisibleSumPairs.java
@@ -0,0 +1,64 @@
+package DAY73;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int sum=0,count=0;
+        for (int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                sum=0;
+                sum=ar.get(i)+ar.get(j);
+                if(sum%k==0) count++;
+            }
+        }
+        return count;
+
+    }
+}
+public class DivisibleSumPairs {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
---
+++ DAY73/Question
@@ -0,0 +1,40 @@
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+
+
+
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Constraints
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output
+
+ 5
+Explanation
+
+Here are the  valid pairs when :
+
