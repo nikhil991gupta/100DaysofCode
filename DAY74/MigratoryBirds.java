3d15f6d53c98a7d5cbfffdacba5ba7d803ed5a1f
nikhil991gupta <nikhil991gupta@gmail.com>
2022-12-23 22:53:10 +0530

    Migratory Birds

---
+++ DAY74/MigratoryBirds.java
@@ -0,0 +1,59 @@
+package DAY74;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+    int frequency = 0;
+    int result = 0;
+    for(int i=0; i<=Collections.max(arr); i++)
+    {
+       int f = Collections.frequency(arr, i);
+       if(f>0 && f>frequency){
+           frequency = f;
+           result = i;
+       }
+    }
+return result;
+}
+
+}
+
+public class MigratoryBirds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
