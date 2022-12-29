2001fd15f152b774b70f31cec878ad2eeaf8110c
nikhil991gupta <nikhil991gupta@gmail.com>
2022-12-29 23:18:17 +0530

    Counting Valleys

---
+++ DAY79/CountingValleys.java
@@ -0,0 +1,27 @@
+package DAY79;
+
+import java.io.*;
+import java.util.*;
+
+public class CountingValleys {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        String s = in.next();
+        
+        int level = 0;
+        int valleys = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'U'){
+                level++;
+            }else if(s.charAt(i) == 'D'){
+                if(level == 0){
+                    valleys++;
+                }
+                level--;
+            }
+        }
+        System.out.println(valleys);
+    }
+}
