dffe96fcac17b1dcb47714019e24a13d331820e3
nikhil991gupta <nikhil991gupta@gmail.com>
2023-01-04 21:29:00 +0530

    CamelCase Java

---
+++ DAY85/CamelCase.java
@@ -0,0 +1,46 @@
+package DAY85;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+     return ((int)s.chars().filter((c)->Character.isUpperCase(c)).count()+1);
+    }
+    }
+
+
+
+public class CamelCase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
