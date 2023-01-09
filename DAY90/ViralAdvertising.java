e76200ee13b39cade85ee4fb5b83081fa64611de
nikhil991gupta <nikhil991gupta@gmail.com>
2023-01-09 21:12:23 +0530

    Viral Advertising

---
+++ DAY90/ViralAdvertising.java
@@ -0,0 +1,55 @@
+package DAY90;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+public static int viralAdvertising(int n) {
+    // Write your code here
+    int i=1;
+    int like=2;
+    int share=5;
+    int totalLike=2;
+        while (n!=i) {
+            i++;
+            share=like*3;
+            like=share/2;
+            totalLike+=like;
+        }
+        return totalLike;
+    }
+
+}
+
+public class ViralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
