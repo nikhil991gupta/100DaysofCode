ef6bcb9cd82bcec4af7ebb2ea89615aa42f6c694
nikhil991gupta <nikhil991gupta@gmail.com>
2023-01-10 22:37:35 +0530

    Designer PDF Viewer

---
+++ DAY91/DesignerPDFViewer.java
@@ -0,0 +1,60 @@
+package DAY91;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+    // Write your code here
+    int max=0;
+        for(int i=0;i<word.length();i++){
+            if(h.get(word.charAt(i)-97) > max){
+                max =h.get(word.charAt(i)-97);
+            }
+        }
+        return max*word.length();
+    }
+
+}
+
+
+
+public class DesignerPDFViewer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
