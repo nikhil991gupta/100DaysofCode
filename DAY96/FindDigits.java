e6a39bbceab28caf84fac6925e16cf2d0d5b8d84
nikhil991gupta <nikhil991gupta@gmail.com>
2023-01-15 21:18:18 +0530

    Find digits

---
+++ DAY96/FindDigits.java
@@ -0,0 +1,63 @@
+package DAY96;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+ public static int findDigits(int n) {
+    // Write your code here
+    int num =n;
+    int c=0;
+    while(num!=0){
+        int rem = num%10;
+        if(rem!=0 && n%rem ==0)
+            c++;
+        num= num/10;
+    }
+return c;
+    }
+
+}
+
+public class FindDigits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
