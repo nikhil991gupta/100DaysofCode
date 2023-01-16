127345d76d892fdda2ab8fbec0ae9cfd96f471b0
nikhil991gupta <nikhil991gupta@gmail.com>
2023-01-17 04:02:39 +0530

    .

--- DAY97/cutthesticks.java
+++ DAY97/cutthesticks.java
@@ -40,7 +40,7 @@ class Result {
 
 }
 
-public class Solution {
+public class cutthesticks {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
