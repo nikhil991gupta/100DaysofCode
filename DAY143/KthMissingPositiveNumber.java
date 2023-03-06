0ab3ba0db12998332bf8e862343b745dba441622
nikhil991gupta <nikhil991gupta@gmail.com>
2023-03-07 01:46:00 +0530

    1539. Kth Missing Positive Number

---
+++ DAY143/KthMissingPositiveNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
